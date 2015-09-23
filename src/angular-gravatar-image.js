10f6882113565cb77650691f58f1e3101eb1f47b
Budi Irawan <deerawan@gmail.com>
2015-09-23 07:15:43 +0700

    add email scope

--- src/angular-gravatar-image.js
+++ src/angular-gravatar-image.js
@@ -8,11 +8,22 @@
   function gravatarImage() {
     var directive = {
       restrict: 'EA',
-      templateUrl: 'template/angular-gravatar-image.html'
+      scope: {
+        email: '@'
+      },
+      template: '<div><img src="{{url}}" /></div>',
+      controller: gravatarImageController
+      // link: function(scope) {
+      //   scope.url = 'http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50';
+      // }
     };
 
     return directive;
+  }
 
+  gravatarImageController.$inject = ['$scope'];
+  function gravatarImageController($scope) {
+    $scope.url = 'http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50';
   }
 
 }());
--- test/spec/angular-gravatar-image.spec.js
+++ test/spec/angular-gravatar-image.spec.js
@@ -11,20 +11,21 @@ describe('angular-gravatar-image', function() {
   beforeEach(inject(function(_$rootScope_, _$compile_, $httpBackend) {
     $compile = _$compile_;
     $scope = _$rootScope_;
-
-    // $httpBackend.whenGET('angular-gravatar-image.html').passThrough();
   }));
 
-  // afterEach(function() {
-  //    $httpBackend.verifyNoOutstandingExpectation();
-  //    $httpBackend.verifyNoOutstandingRequest();
-  //  });
+  // it('should initialize', function() {
+  //   element = angular.element('<gravatar-image></gravatar-image>');
+  //   element = $compile(element)($scope);
+  //   $scope.$apply();
 
-  it('should initialize', function() {
-    element = angular.element('<gravatar-image></gravatar-image>');
+  //   expect(element.find('img').length).toEqual(1);
+  // });
+
+  it('should initialize with email', function() {
+    element = angular.element('<gravatar-image email="deerawan@gmail.com"></gravatar-image>');
     element = $compile(element)($scope);
     $scope.$apply();
 
-    expect(element.find('img').length).toEqual(1);
+    expect(element.find('div').html()).toEqual('http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50');
   });
 });
