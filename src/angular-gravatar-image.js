379a4ea120a94caed5072fab30b0b6a0aa160780
Budi Irawan <deerawan@gmail.com>
2015-10-02 23:25:41 +0700

    * add SSL support and test * move URL from controller to link function

--- src/angular-gravatar-image.js
+++ src/angular-gravatar-image.js
@@ -18,8 +18,13 @@
 
     return directive;
 
-    function link(scope, element, attrs) {
+    function link(scope, element, attrs, controller) {
+      var url = 'http://www.gravatar.com';
       var parameters = '';
+
+      if (attrs.ssl) {
+        url = 'https://secure.gravatar.com';
+      }
 
       if (attrs.size) {
         parameters += 'size=' + attrs.size;
@@ -37,19 +42,18 @@
         parameters += '&rating=' + attrs.rating;
       }
 
-      if (parameters != '') {
-        scope.url = scope.url + '?' + parameters;
+      scope.url = url + '/avatar/' + controller.emailHash;
+
+      if (parameters !== '') {
+        scope.url += '?' + parameters;
       }
-
-
     }
   }
 
   GravatarImageController.$inject = ['$scope', 'md5'];
 
   function GravatarImageController($scope, md5) {
-    var md5Hash = md5.encrypt($scope.email);
-    $scope.url = 'http://www.gravatar.com/avatar/' + md5Hash;
+    this.emailHash = md5.encrypt($scope.email);
   }
 
 }());
--- test/spec/angular-gravatar-image.spec.js
+++ test/spec/angular-gravatar-image.spec.js
@@ -8,18 +8,10 @@ describe('angular-gravatar-image', function() {
 
   beforeEach(module('template'));
 
-  beforeEach(inject(function(_$rootScope_, _$compile_, $httpBackend) {
+  beforeEach(inject(function(_$rootScope_, _$compile_) {
     $compile = _$compile_;
     $scope = _$rootScope_;
   }));
-
-  // it('should initialize', function() {
-  //   element = angular.element('<gravatar-image></gravatar-image>');
-  //   element = $compile(element)($scope);
-  //   $scope.$apply();
-
-  //   expect(element.find('img').length).toEqual(1);
-  // });
 
   it('should initialize with email', function() {
     element = angular.element('<gravatar-image email="deerawan@gmail.com"></gravatar-image>');
@@ -35,5 +27,13 @@ describe('angular-gravatar-image', function() {
     $scope.$apply();
 
     expect(element.find('img').attr('src')).toEqual('http://www.gravatar.com/avatar/71a6523c5915c0ee79a79a137c1b2be0?size=200');
+  });
+
+  it('should support ssl url', function() {
+    element = angular.element('<gravatar-image email="deerawan@gmail.com" ssl=true></gravatar-image>');
+    element = $compile(element)($scope);
+    $scope.$apply();
+
+    expect(element.find('img').attr('src')).toEqual('https://secure.gravatar.com/avatar/71a6523c5915c0ee79a79a137c1b2be0');
   });
 });
