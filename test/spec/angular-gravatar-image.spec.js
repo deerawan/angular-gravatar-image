d3068b3c3b79dfad2aa1b74e33fd3633901da395
Budi Irawan <deerawan@gmail.com>
2015-10-02 23:34:02 +0700

    remove template

--- test/spec/angular-gravatar-image.spec.js
+++ test/spec/angular-gravatar-image.spec.js
@@ -6,8 +6,6 @@ describe('angular-gravatar-image', function() {
 
   beforeEach(module('bgn.GravatarImage'));
 
-  beforeEach(module('template'));
-
   beforeEach(inject(function(_$rootScope_, _$compile_) {
     $compile = _$compile_;
     $scope = _$rootScope_;
@@ -42,6 +40,6 @@ describe('angular-gravatar-image', function() {
     element = $compile(element)($scope);
     $scope.$apply();
 
-    expect(element.find('img').attr('src')).toEqual('https://secure.gravatar.com/avatar/71a6523c5915c0ee79a79a137c1b2be0?size=200&default=retro&forcedefault=y&rating=pg');
+    expect(element.find('img').attr('src')).toEqual('http://www.gravatar.com/avatar/71a6523c5915c0ee79a79a137c1b2be0?size=200&default=retro&forcedefault=y&rating=pg');
   });
 });
